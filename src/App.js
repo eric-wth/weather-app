5f3a8a1f85b7aa355f407ce5388d723d6bf9dea3
eric-waithaka <eric.email>
2022-10-14 21:09:22 +0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState('');
+  const [temp, setTemp] = useState(0);
+
+  const submitData = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5a99290f7b68f1481988241c36b7da59&units=metric`)
+      .then((response) => {
+        console.log(response);
+        setTemp(response.data.main.temp);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Get the temperature of any country below</h1>
+      <br />
+      <form onSubmit={submitData}>
+        <label htmlFor='city-name'>City name:</label>
+        <input
+          type='text'
+          name='city_name'
+          id='city-name'
+          placeholder='Enter city name here'
+          onChange={(e) => {setCity(e.target.value)}}
+        />
+        <button className='button'>Get temp</button>
+      </form>
+
+      <div>{temp} °C</div> {/* temp display goes here */}
     </div>
-  );
+  )
 }
 
 export default App;
